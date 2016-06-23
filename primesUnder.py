d0b75424932dc87215f1682d5161f7964b4d2752
campadjc <david.campa@rackspace.com>
2016-06-23 09:34:56 -0500

    Create primesUnder.py

---
+++ primesUnder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def isPrime(x):
+  for i in xrange(2,x):
+    if ( x%i == 0 ):
+      return False
+    #end if
+  #end for
+  return True
+#end isPrime()
+
+def primesUnder(x):
+  for num in xrange(2,x):
+    if isPrime(num):
+      print num
+  #end for
+#end primesUnder()
+
+primesUnder(1000)
